d516de665e8c20fd007f40dc9bd17365ac566959
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-13 21:17:58 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (good)
+ */
+int main(void)
+{
+	printf("\"with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
