d5cd75c31188432284b35739536775b6d78516c1
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-16 11:10:46 -0700

    numbers to base ten

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - print all single digit numbers under base ten
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
