9fab686e35a6088888aafbfc513d34bfc7a91362
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-16 07:57:42 -0700

    alphabets in small cases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - write alphabets in lower cases
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
