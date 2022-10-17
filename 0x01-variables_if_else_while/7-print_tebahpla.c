2f2219fb16abf5d9268d607135b342a2fdf790bf
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-17 00:19:34 -0700

    revers order of alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - write alphabets in reverse oder
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha++)
+	{
+		alpha = tolower(alpha);
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
