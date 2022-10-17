a5a29d166d9680e36bf071ab5731eebd7a923de0
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-17 01:33:12 -0700

    single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - print single numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int s_num;
+
+	for (s_num = 0; s_num <= 9; s_num++)
+	{
+		putchar((s_num % 10) + '0');
+		if (s_num == 0)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
