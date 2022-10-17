92cf41dbcb55e2440e9bd012c725912dabbe963e
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-17 00:55:16 -0700

    numbers under base sixteen

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,18 @@
 int main(void)
 {
 	int num;
+	char alpha;
 
-	for (num = '0'; num <= 15 ; num++)
+	for (num = 0; num < 10 ; num++)
 	{
 		putchar(num);
 	}
+
+	for (alpha = 'a'; alpha < 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+
 	putchar('\n');
 
 	return (0);
