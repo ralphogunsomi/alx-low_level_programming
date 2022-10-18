e75c737fd7e89c0467140660237aa94573009caf
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-17 19:34:50 -0700

    print two different digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,7 +19,7 @@ int main(void)
 		for (n2 = n1 + 1; n2 < 10; n2++)
 		{
 			putchar((n1 % 10) + '0');
-			putcher((n2 % 10) + '0');
+			putchar((n2 % 10) + '0');
 			/*using logical operations*/
 
 			if (n1 == 8 && n2 == 9)
