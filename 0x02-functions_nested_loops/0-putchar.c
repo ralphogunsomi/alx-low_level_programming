5532ebbb5183148b51af70f88168371cf7b424d3
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-19 07:38:27 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - this program prints putchar.
+ * main - prints putchar.
  *
  * Return: always 0
  */
 
 int main(void)
 {
-	_putchar('_');
+	_putchar('1');
 
 	_putchar('q');
 
