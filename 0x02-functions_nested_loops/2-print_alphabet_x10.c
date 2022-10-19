aab317bf21eea51aed8d0ed5964867314f9bdbd9
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-19 08:19:44 -0700

    repeat alphabets ten time

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int get;
+	int count = 0;
 	char lett;
 
-	int get = 0;
-
-	while (get++ <= 9)
+	while (count++ <= 9)
 	{
 		for (lett = 'a'; lett <= 'z'; lett++)
 			_putchar(lett);
 
-		_putcher('\n');
+		_putchar('\n');
 	}
 }
