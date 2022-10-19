6979fc1b082ae69be41599f074ccb5ad40eabc5d
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-19 07:09:21 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,22 +8,13 @@
 int main(void)
 {
 	_putchar('_');
-
-       	_putchar('p');
-
+	_putchar('p');
 	_putchar('u');
-
 	_putchar('v');
-
 	_putchar('w');
-
 	_putchar('e');
-
 	_putchar('y');
-
 	_putchar('x');
-
 	_putchar('\n');
-
 	return (0);
 }
