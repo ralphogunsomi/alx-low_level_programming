03508710098b95dbbfc1487de5d13cc8022c45bc
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-25 00:47:39 -0700

    string reversal

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,18 +2,48 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- *main - Prints all single digits of base 10 followed by new line
- *Return: returns 0
+ *rev_string - Reverse a string
+ *@s: String to reverse
+ *Return: Nothing
  */
-int main(void)
+void rev_string(char *s)
 {
-	int digit;
+	int i = 0, length;
 
-	for (digit = '0'; digit <= '9'; digit++)
+	length = _strlen(s) - 1;
+	while (length > i)
 	{
-		putchar(digit);
+		swap_char(s + length, s + i);
+		i++;
+		length--;
 	}
-	putchar('\n');
+}
 
-	return (0);
+
+/**
+ *_strlen - returns the length of a string
+ *@s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+	int count, inc;
+
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+	return (inc);
+}
+
+/**
+ *swap_char - swap two characters
+ *@a: first character
+ *@b: second character
+ *Return: nothing
+ */
+void swap_char(char *a, char *b)
+{
+	char avc = *a;
+	*a = *b;
+	*b = avc;
 }
