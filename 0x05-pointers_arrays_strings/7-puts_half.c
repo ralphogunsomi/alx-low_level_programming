a116382be087fdfd1de0b25b993d4d4b80706c95
ralphogunsomi <ralphogunsomi@gmail.com>
2022-10-25 02:00:11 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *puts_half - puts half of a string
+ *@str: string
+ *Return:void
+ */
+void puts_half(char *str)
+{
+	if (len % 2 == 0)
+		len = len / 2;
+
+	else
+		len = (len + 1) / 2;
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
+
+/**
+ *_strlen - returns the length of a string
+ *@s: string
+ * Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+	inc = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	inc++;
+
+	return (inc);
+}
